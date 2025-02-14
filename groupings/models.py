5b1e08f35b4b71beb0e8346abd479f07538c7f75
Brandeau <diego.andrade.y@gmail.com>
2025-02-14 14:05:43 -0300

    fix: syntax of model

--- groupings/models.py
+++ groupings/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class Groupings:
+class Groupings(models.Model):
     name = models.CharField(max_length=50)
     type = models.CharField(max_length=50)
 
