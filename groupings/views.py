7891bbcabf91c05f9ef4cf2002e4cc1f6a500edb
Brandeau <diego.andrade.y@gmail.com>
2025-02-21 12:15:00 -0300

    feat: create groupings section

--- groupings/views.py
+++ groupings/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.views.generic.base import TemplateView
+from groupings.models import Groupings
+from django.shortcuts import render, get_object_or_404
 
-# Create your views here.
+
+class GroupingsListView(TemplateView):
+    template_name = 'groupings/groupings_list.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        groupings = Groupings.objects.all()
+        context['groupings'] = groupings
+
+        return context
+    
+class GroupingsDetailsView(TemplateView):
+    template_name = 'groupings/groupings_detail.html'
+
+    def get(self, request, id, *args, **kwargs):
+        grouping = get_object_or_404(Groupings, pk=id)
+        context = {'grouping': grouping}
+        return render(request, self.template_name, context)
+        
