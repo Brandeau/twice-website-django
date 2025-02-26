e7e5337625da1fc982ec63965c7135f23cd887ee
Brandeau <diego.andrade.y@gmail.com>
2025-02-26 20:05:55 -0300

    feat: create releases page

--- releases/views.py
+++ releases/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 from django.views.generic.base import TemplateView
 from releases.models import ReleaseGroup, Release
-from tracks.models import Track
+from tracks.models import TrackNum, Track
 from django.shortcuts import render, get_object_or_404
 class ReleasesListView(TemplateView):
     template_name = 'releases/releases_list.html'
@@ -20,19 +20,12 @@ class ReleasesDetailsView(TemplateView):
 
     def get(self, request, id, *args, **kwargs):
         releaseGroup = get_object_or_404(ReleaseGroup, pk=id)
-
-        releases = Release.objects.filter(release_group_id=releaseGroup)     
-
-        releases_tracks = {}       
-
-        for release in releases:
-            releases_tracks[release] = {
-                'name': release,
-                'tracks': {Track.objects.filter(releases=release)}
-            }
-        context = {'releases': releases, 
-                   'release_group': releaseGroup,
-                   'release_tracks': releases_tracks    
-                   }
+        releases = Release.objects.filter(release_group=releaseGroup)
+        
+        tracks = TrackNum.objects.all()
+            
+        context = {'releases': releases,
+                   'tracks': tracks                 }
+        
         return render(request, self.template_name, context)
         
