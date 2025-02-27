b0489ae26505a110baa93b8d0bfcc31a7690908b
Brandeau <diego.andrade.y@gmail.com>
2025-02-27 11:55:55 -0300

    feat: added admin capabilities to releases

--- releases/admin.py
+++ releases/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import ReleaseGroup, Release, ReleaseFormat, ReleaseGroupType
 
 # Register your models here.
+class ReleaseGroupAdmin(admin.ModelAdmin):
+    model = ReleaseGroup
+    list_display = ['name', 'grouping', 'type']
+
+admin.site.register(ReleaseGroup, ReleaseGroupAdmin)
+
+class ReleaseAdmin(admin.ModelAdmin):
+    model = Release
+    list_display = ['title', 'release_year', 'format', 'country', 'release_group', 'annotation']
+  
+
+admin.site.register(Release, ReleaseAdmin)
